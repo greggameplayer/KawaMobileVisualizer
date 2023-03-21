a6eb775ea016bc5cc94db5acd09a36df761aa002
victormarit <victormarit.95@gmail.com>
2023-03-21 22:38:06 +0100

    test sqlite

--- test/service/customer_save_sqlite_test.dart
+++ test/service/customer_save_sqlite_test.dart
@@ -1,40 +1,55 @@
-
-
-import 'package:flutter/widgets.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:kawa_mobile_visualizer/models/Customer.dart';
 import 'package:kawa_mobile_visualizer/services/customer_save_sqlite.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:test/test.dart';
 
-Future<void> main() async {
+
+void sqfliteTestInit() {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
+}
+
+
+Future main() async {
+  sqfliteTestInit();
   WidgetsFlutterBinding.ensureInitialized();
-  await deleteDb();
 
-  CustomerSave customer = CustomerSave(id: 1, email: "email", token: "token");
-  await insertUserinSqlite(customer);
 
-  var customers = await getCustomersInDb(customer);
-  test('Should return a list of one CustomerSave', (){
-    expect(customers.length, 1);
-    expect(customers[0].id, 1);
-    expect(customers[0].email, "email");
-    expect(customers[0].token, "token");
+
+  CustomerSave customerSave = CustomerSave(id: 1, email: "email", token: "token");
+  test("Customer should have been initialized", () => {
+    expect(customerSave.id, 1),
+    expect(customerSave.email, "email"),
+    expect(customerSave.token, "token")
   });
 
-  var customer1 = CustomerSave(id: 1, email: "victormarit.95@gmail.com", token: "1234566Kafetoken");
-  updateUserinSqlite(customer1);
-  var customerIdentifiedById = await getCustomerInDbById(customer.id);
-  test('Should return CustomerSave updated', (){
-    expect(customerIdentifiedById.id, 1);
-    expect(customerIdentifiedById.email, "victormarit.95@gmail.com");
-    expect(customerIdentifiedById.token, "1234566Kafetoken");
+  insertUserinSqlite(customerSave);
+
+  List<CustomerSave> customers = await getCustomersInDb();
+  test("Should return return a list of one CustomerSave", () => {
+    expect(customers.length, 1),
+    expect(customers[0].id, 1),
+    expect(customers[0].email, "email"),
+    expect(customers[0].token, "token")
   });
 
+  CustomerSave customerSave1 = CustomerSave(id: 1, email: "mspr@gmail.com", token: "faketoken1");
+  updateUserinSqlite(customerSave1);
 
-  await deleteUserinSqlite(customer1);
-  customers = await getCustomersInDb(customer);
-  test('Should return an empty list of CustomerSave', (){
-    expect(customers.length, 0);
+  CustomerSave customerSave2 = await getCustomerInDbById(1);
+  test("Should have update customerSave in db", () => {
+    expect(customerSave2.id, 1),
+    expect(customerSave2.email, "mspr@gmail.com"),
+    expect(customerSave2.token, "faketoken1"),
   });
 
-  await deleteDb();
+  deleteUserinSqlite(customerSave2);
+
+  List<CustomerSave> customers2 = await getCustomersInDb();
+  test("Should return return a list of zero CustomerSave", () => {
+    expect(customers2.length, 0),
+  });
+
 }
